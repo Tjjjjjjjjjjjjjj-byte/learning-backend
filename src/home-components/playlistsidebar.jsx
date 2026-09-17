be39da3ba6b21a0d8a9df4cd811899656dd24959
Tjjjjjjjjjjjjjj-byte <tjdanielaliado606@gmail.com>
2026-09-17 19:51:37 +0800

    massive ui/ux overhaul

--- src/home-components/playlistsidebar.jsx
+++ src/home-components/playlistsidebar.jsx
@@ -3,16 +3,17 @@ import CreateNewPlaylist from "./playlists/createnewplaylist";
 import Minimize from "./playlists/minimize";
 import Playlist from "./playlists/playlistComponent";
 import Maximize from "./playlists/maximize";
+import "../styling/sidebar.css";
 function PlaylistSidebar() {
   const [minimized, setMinimized] = useState(true);
   const [maximized, setMaximized] = useState(false);
   return (
-    <>
+    <aside className={minimized ? "sidebar minimized" : "sidebar"}>
       <Minimize setMinimized={setMinimized} minimized={minimized} />
       <CreateNewPlaylist minimized={minimized} maximized={maximized} />
       <Playlist minimized={minimized} maximized={maximized}/>
       <Maximize minimized={minimized} setMaximized={setMaximized} maximized={maximized}/>
-    </>
+    </aside>
   );
 }
 export default PlaylistSidebar;
