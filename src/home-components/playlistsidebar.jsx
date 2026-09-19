2b8f8aae18d3bfc0bb13cc99bac351f35eb649b9
Tjjjjjjjjjjjjjj-byte <tjdanielaliado606@gmail.com>
2026-09-19 08:15:58 +0800

    asdfhasdf

--- src/home-components/playlistsidebar.jsx
+++ src/home-components/playlistsidebar.jsx
@@ -5,20 +5,44 @@ import Playlist from "./playlists/playlistComponent";
 import Maximize from "./playlists/maximize";
 import "../styling/sidebar.css";
 function PlaylistSidebar() {
-  const [createOptionsHidden, setCreateOptionsHidden] = useState(false)
+  const [createOptionsHidden, setCreateOptionsHidden] = useState(false);
   const [minimized, setMinimized] = useState(true);
   const [maximized, setMaximized] = useState(false);
-  return (
-    !maximized ? (<aside className={minimized ? "sidebar minimized" : "sidebar"}>
+  return !maximized ? (
+    <aside className={minimized ? "sidebar minimized" : "sidebar"}>
       <Minimize setMinimized={setMinimized} minimized={minimized} />
-      <CreateNewPlaylist minimized={minimized} maximized={maximized} createOptionsHidden={createOptionsHidden} setCreateOptionsHidden={setCreateOptionsHidden}/>
-      <Maximize minimized={minimized} setMaximized={setMaximized} maximized={maximized}/>
-      <Playlist minimized={minimized} maximized={maximized}/>
-    </aside>) : <aside className="maximized">
-      <Minimize setMinimized={setMinimized} minimized={minimized} maximized={maximized} />
-      <CreateNewPlaylist minimized={minimized} maximized={maximized} createOptionsHidden={createOptionsHidden} setCreateOptionsHidden={setCreateOptionsHidden}/>
-      <Maximize minimized={minimized} setMaximized={setMaximized} maximized={maximized}/>
-      <Playlist minimized={minimized} maximized={maximized}/>
+      <CreateNewPlaylist
+        minimized={minimized}
+        maximized={maximized}
+        createOptionsHidden={createOptionsHidden}
+        setCreateOptionsHidden={setCreateOptionsHidden}
+      />
+      <Maximize
+        minimized={minimized}
+        setMaximized={setMaximized}
+        maximized={maximized}
+      />
+      <Playlist minimized={minimized} maximized={maximized} />
+    </aside>
+  ) : (
+    <aside className="maximized">
+      <Minimize
+        setMinimized={setMinimized}
+        minimized={minimized}
+        maximized={maximized}
+      />
+      <CreateNewPlaylist
+        minimized={minimized}
+        maximized={maximized}
+        createOptionsHidden={createOptionsHidden}
+        setCreateOptionsHidden={setCreateOptionsHidden}
+      />
+      <Maximize
+        minimized={minimized}
+        setMaximized={setMaximized}
+        maximized={maximized}
+      />
+      <Playlist minimized={minimized} maximized={maximized} />
     </aside>
   );
 }
